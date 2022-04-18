77c1a858d55840f664a32d4263dcd341e5ff227a
saman <saman.rafiee.rs@gmail.com>
2022-04-19 00:49:25 +0430

    create all blog view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from blog.models import Blog
+from blog.serializers import BlogModelSerializer
+
+
+class GetAllBlog(APIView):
+    def get(self, request):
+        query = Blog.objects.all()
+        serializers = BlogModelSerializer(query, many=True)
+        return Response(serializers.data, status=status.HTTP_200_OK)
